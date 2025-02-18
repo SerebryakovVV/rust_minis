bf275a407caca3bff4b56431fad8311ef2b3ca5c
SerebryakovVV <serebryakov.valentin@gmail.com>
2025-02-19 02:33:20 +0700

    db testing

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,45 @@
 use rusqlite as rsql;
 
 
-fn main() {
+#[derive(Debug)]
+struct User {
+    name: String
+}
 
-    let path = "../tasks.db3";
-    let db = match rsql::Connection::open(path) {
-        Err(_) => panic!(),
-        Ok(con) => con
-    };
+fn main() -> rsql::Result<()> {
 
-    let query_result = match db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY,name  TEXT NOT NULL)", ()) {
-        Err(_) => panic!(),
-        Ok(r) => r 
-    };
+    // let path = "tasks.db3";
+    // let db = match rsql::Connection::open(path) {
+    //     Err(_) => panic!(),
+    //     Ok(con) => con
+    // };
 
     
+    // db.execute("DROP TABLE users", ())?;
+
+    // let query_result = match db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY,name  TEXT NOT NULL)", ()) {
+    //     Err(e) => {println!("{}", e); panic!("first");},
+    //     Ok(r) => r 
+    // };
+
+    // let query_result = match db.execute("insert into users (name) values ('valentin')", ()) {
+    //     Err(e) => {println!("{}", e); panic!("first");},
+    //     Ok(r) => r 
+    // };
+
+    // let mut statement = db.prepare("select name from users")?;
+    // let query_result = statement.query_map([], |el| {
+    //     Ok(User{name:el.get(0)?})
+    // })?;
+
+  
+
+    // for u in query_result {
+    //     println!("{:?}", u);
+        
+    // }
 
     println!("Hello, world!");
+
+    Ok(())
 }
